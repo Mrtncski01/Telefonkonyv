6cc1f5b065ecec02fb470160401f9a9523f72239
Mrtncski01 <150520755+Mrtncski01@users.noreply.github.com>
2025-05-08 21:41:10 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,9 @@
 #include <iostream>
-#include "Name.h"
-#include "Doctor.h"
-#include "Patient.h"
 #include "Phonebook.h"
+#include "menu.h"
 
 int main() {
-    Name name1("John", "Doe", "Johnny");
-    std::cout << "Firstname: " << name1.getFirstname() << std::endl;
-    std::cout << "Lastname: " << name1.getLastname() << std::endl;
-    std::cout << "Nickname: " << name1.getNickname() << std::endl;
-
-    Doctor doc(name1, "12345", "54321", "Budapest", "Kardiológia", "Kórház");
-    std::cout << "\nDoctor info:\n";
-    doc.print(std::cout);
-
-    Name name2("Jane", "Smith", "Janie");
-    Patient pat(name2, "98765", "56789", "Szeged", "Asztma", "Dr. House");
-    std::cout << "\nPatient info:\n";
-    pat.print(std::cout);
-
-    Phonebook pb;
-    pb.addContact(new Doctor(name1, "11111", "22222", "Budapest", "Sebészet", "Kórház2"));
-    pb.addContact(new Patient(name2, "33333", "44444", "Pécs", "Allergia", "Dr. Wilson"));
-
-    std::cout << "\nPhonebook list:\n";
-    pb.listContacts();
-
+    Menu m;
+    m.run();
     return 0;
 }
