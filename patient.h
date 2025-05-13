fbcbadb4080a64485b796b764159162dd0ab5b66
Mrtncski01 <150520755+Mrtncski01@users.noreply.github.com>
2025-05-14 01:27:46 +0200

    Update patient.h

--- patient.h
+++ patient.h
@@ -1,5 +1,5 @@
-#ifndef TELEFONKONYV_PATIENT_H
-#define TELEFONKONYV_PATIENT_H
+#ifndef PHONEBOOK_PATIENT_H
+#define PHONEBOOK_PATIENT_H
 #include "contact.h"
 
 class Patient : public Contact {
@@ -8,14 +8,17 @@ private:
     std::string condition;
 public:
     Patient(const Name& newName, const std::string& newPrivatePhone,
-    const std::string& newWorkPhone, const std::string& newAddress,
-    const std::string& newCondition, const std::string& newDoctorName) : Contact(newName, newPrivatePhone, newWorkPhone, newAddress),
-    doctorName(newDoctorName), condition(newCondition) {}
+            const std::string& newWorkPhone, const std::string& newAddress,
+            const std::string& newCondition, const std::string& newDoctorName) : Contact(newName, newPrivatePhone, newWorkPhone, newAddress),
+                                                                                 doctorName(newDoctorName), condition(newCondition) {}
     std::string getCondition() const;
     std::string getdoctorName() const;
     void setCondition(std::string newCondition);
     void setdoctorName(std::string newdoctorName);
     void print(std::ostream& os) const;
+    std::string serialize() const;
+    //~Patient() {}
 };
 std::ostream& operator<<(std::ostream& os, const Patient& p);
-#endif //TELEFONKONYV_PATIENT_H
+
+#endif //PHONEBOOK_PATIENT_H
