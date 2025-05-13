e67fc96faf59a252a6a789392334e9fccbae9672
Mrtncski01 <150520755+Mrtncski01@users.noreply.github.com>
2025-05-14 01:27:08 +0200

    Update name.h

--- name.h
+++ name.h
@@ -1,23 +1,24 @@
-#ifndef TELEFONKONYV_NAME_H
-#define TELEFONKONYV_NAME_H
+#ifndef PHONEBOOK_NAME_H
+#define PHONEBOOK_NAME_H
 #include <iostream>
+
 class Name {
 private:
     std::string firstname;
     std::string lastname;
     std::string nickname;
 public:
-    Name()=default;
-    Name(const std::string& newfirstname, const std::string& newlastname, std::string  newnickname) : firstname(newfirstname), lastname(newlastname), nickname(newnickname) {}
+    Name() {}
+    Name(const std::string& newfirstname, const std::string& newlastname,const std::string& newnickname) : firstname(newfirstname), lastname(newlastname), nickname(newnickname) {}
     std::string getFirstname() const;
     std::string getLastname() const;
     std::string getNickname() const;
     void setFirstname(const std::string& newFirstname);
     void setLastname(const std::string& newLastname);
     void setNickname(const std::string& newNickname);
+    void namePrint(std::ostream& os) const;
     ~Name() {}
 };
-
 std::ostream& operator<<(std::ostream& os, const Name& name);
 
-#endif //TELEFONKONYV_NAME_H
+#endif //PHONEBOOK_NAME_H
