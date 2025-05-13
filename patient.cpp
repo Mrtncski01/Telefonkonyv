e9ca64e00bcf2d614266ae777bd296453a1459f2
Mrtncski01 <150520755+Mrtncski01@users.noreply.github.com>
2025-05-14 01:27:26 +0200

    Update patient.cpp

--- patient.cpp
+++ patient.cpp
@@ -1,4 +1,5 @@
 #include "patient.h"
+#include "memtrace.h"
 
 std::string Patient::getCondition() const {
     return condition;
@@ -15,10 +16,21 @@ void Patient::setdoctorName(std::string newdoctorName) {
 }
 
 void Patient::print(std::ostream &os) const {
-    os << name.getFirstname() << " " << name.getLastname()<< " " << '(' << name.getNickname()<< ')' <<" " << condition << " " << doctorName << std::endl;
+    name.namePrint(os);
+    os << "CIM: " << getAddress() << '\n'
+       << "MUNKAHELYI SZAM: " << getWorkPhone() << '\n'
+       << "PRIVAT SZAM:" << getPrivatePhone() << '\n'
+       << "EGESZSEGUGYI ALLAPOT: " << getCondition() << '\n'
+       << "KEZELOORVOS NEVE:" << getdoctorName() << '\n';
 }
 
 std::ostream& operator<<(std::ostream& os, const Patient& p) {
     p.print(os);
     return os;
+}
+
+std::string Patient::serialize() const {
+    std::ostringstream oss;
+    oss << '0' << '|' << name.getLastname() << " | " << name.getFirstname() << " | " << name.getNickname() << " | " << address << " | " << workPhone << " | " << privatePhone << " | " << condition << " | " << doctorName << " | $";
+    return oss.str();
 }
