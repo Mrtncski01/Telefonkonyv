a3c5220928a72996c8c6505fa106a2c2e3a04cb0
Mrtncski01 <150520755+Mrtncski01@users.noreply.github.com>
2025-05-14 01:23:15 +0200

    Update doctor.h

--- doctor.h
+++ doctor.h
@@ -1,5 +1,5 @@
-#ifndef TELEFONKONYV_DOCTOR_H
-#define TELEFONKONYV_DOCTOR_H
+#ifndef PHONEBOOK_DOCTOR_H
+#define PHONEBOOK_DOCTOR_H
 #include "contact.h"
 
 class Doctor : public Contact {
@@ -10,12 +10,15 @@ public:
     Doctor(const Name& newName, const std::string& newPrivatePhone,
            const std::string& newWorkPhone, const std::string& newAddress,
            const std::string& newWorkplace, const std::string& newSpecialization) : Contact(newName, newPrivatePhone, newWorkPhone, newAddress),
-           specialization(newSpecialization), workplace(newWorkplace) {}
+                                                                                    specialization(newSpecialization), workplace(newWorkplace) {}
     std::string getSpecialization() const;
     std::string getWorkplace() const;
     void setSpecialization(const std::string newSpecialization);
     void setWorkplace(const std::string newWorkplace);
     void print(std::ostream& os) const;
+    std::string serialize() const;
+    //~Doctor() {}
 };
 std::ostream& operator<<(std::ostream& os, const Doctor& d);
-#endif //TELEFONKONYV_DOCTOR_H
+
+#endif //PHONEBOOK_DOCTOR_H
