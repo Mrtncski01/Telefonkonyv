6ba6f906d2400734bed4acf9702b165c6a36cba9
Mrtncski01 <150520755+Mrtncski01@users.noreply.github.com>
2025-05-17 11:37:38 +0200

    Update patient.h

--- patient.h
+++ patient.h
@@ -2,22 +2,23 @@
 #define PHONEBOOK_PATIENT_H
 #include "contact.h"
 
+//paciens osztaly, ami a kontakt osztalybol szarmazik, orokli a tulajdonsagait
 class Patient : public Contact {
 private:
-    std::string doctorName;
-    std::string condition;
+    std::string doctorName; //kez.orvos neve
+    std::string condition; //eg.ugyi allapota
 public:
     Patient(const Name& newName, const std::string& newPrivatePhone,
             const std::string& newWorkPhone, const std::string& newAddress,
             const std::string& newCondition, const std::string& newDoctorName) : Contact(newName, newPrivatePhone, newWorkPhone, newAddress),
-                                                                                 doctorName(newDoctorName), condition(newCondition) {}
-    std::string getCondition() const;
-    std::string getdoctorName() const;
-    void setCondition(std::string newCondition);
-    void setdoctorName(std::string newdoctorName);
-    void print(std::ostream& os) const;
-    std::string serialize() const;
-    //~Patient() {}
+                                                                                 doctorName(newDoctorName), condition(newCondition) {} //konstruktora
+    std::string getCondition() const; //visszakuldi az eg.ugyi allapotat
+    std::string getdoctorName() const; //visszakuldi a doktor nevet
+    void setCondition(std::string newCondition); //beallitja az eg.allapotat, ez a modositas funkciohoz kell
+    void setdoctorName(std::string newdoctorName); //szinten
+    void print(std::ostream& os) const; // override, kiiratja a a paciens adatait
+    std::string serialize() const; //filebamenteshez kell
+    ~Patient() {} //destruktor
 };
 std::ostream& operator<<(std::ostream& os, const Patient& p);
 
