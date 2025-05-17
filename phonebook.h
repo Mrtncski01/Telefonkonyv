3b7bfcd75d487171df5ada14c4113e29433bdd6e
Mrtncski01 <150520755+Mrtncski01@users.noreply.github.com>
2025-05-17 11:38:09 +0200

    Update phonebook.h

--- phonebook.h
+++ phonebook.h
@@ -2,19 +2,20 @@
 #define PHONEBOOK_PHONEBOOK_H
 #include "dinamicarray.h"
 
+//Phonebook osztaly
 class Phonebook {
 private:
-    DinamicArray d;
+    DinamicArray d;  //a phonebook osztaly leginkabb a felhasznalo-program kommunikacioert felel. a dinamicarray osztaly peldanyat hasznalja fel magaert a modositasokert, konrket muveletekert
 public:
-    Phonebook() {}
-    ~Phonebook() {}
-    void addContact();
-    void listAllContact(std::ostream &os);
-    void removeContact(std::ostream &os);
-    void searchContact(std::ostream &os);
-    void modifyContact(std::ostream& os);
-    void saveToFile();
-    void loadFromFile(const std::string& filename);
+    Phonebook() {} //phonebook osztaly konstruktora
+    ~Phonebook() {} //phonebook osztaly desktruktora
+    void addContact(); //uj kontaktot ad hozza a felhasznalohoz
+    void listAllContact(std::ostream &os); //ha a heterogen kollekcio mar tartalmaz adatokat, akkor kiiratja az osszeset egymas ala, ha nincs kiirja hogy nincs kontkakt
+    void removeContact(std::ostream &os); //a felhasznalo valasztasaval torol egy tetszoleges adatot
+    void searchContact(std::ostream &os); //a felhasznalo beuthet keresztnevet, vezeteknevet, becenevet, vagy privat szamot, es kiiratja az osszes ilyen kontaktot, ami tartalmazza ezt
+    void modifyContact(std::ostream& os); //modosit egy tetszoleges adatot
+    void saveToFile(); //filebamentesert felel
+    void loadFromFile(const std::string& filename); //filebol kiolvasasaert felel
 };
 
 
