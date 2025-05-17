8adb0c483661373bb1583dae2772e08055bf768a
Mrtncski01 <150520755+Mrtncski01@users.noreply.github.com>
2025-05-17 11:35:50 +0200

    Update inputmanaging.h

--- inputmanaging.h
+++ inputmanaging.h
@@ -6,22 +6,22 @@
 #include <stdexcept>
 #include <sstream>
 
-inline int readIndexInput(const std::string& prompt, int minInterval, int maxInterval) {
+inline int readIndexInput(const std::string& prompt, int minInterval, int maxInterval) { //ez felel a helyes index beviteleert
     int value;
     std::cout << prompt;
-    if (!(std::cin >> value)) {
+    if (!(std::cin >> value)) { //megvizsgalja hogy egyaltalan szam-e
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         throw std::invalid_argument("HIBAS A BEMENET, SZAMOT VAROK!");
     }
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    if (value < minInterval || value > maxInterval)
+    if (value < minInterval || value > maxInterval) //megvizsgalja hogy megfelelo tartomanyba valasztott-e szamot
         throw std::out_of_range("HIBAS INDEX!");
     return value;
 }
 
 
-inline std::string readStringInput(const std::string& prompt) {
+inline std::string readStringInput(const std::string& prompt) { //string beviteleert felel
     std::string value;
     const std::string forbiddenChars = "|$";
     bool valueIsValid = false;
@@ -37,7 +37,7 @@ inline std::string readStringInput(const std::string& prompt) {
         } else if (value.empty()) {
             throw std::invalid_argument("URES BEMENET!");
         } else if (value.find_first_of(forbiddenChars) != std::string::npos) {
-            throw std::invalid_argument("TILTOTT KARAKTERT HASZNALTAL: | vagy $");
+            throw std::invalid_argument("TILTOTT KARAKTERT HASZNALTAL: | vagy $"); //megvizsgalja hogy nem irt-e bele tiltott karatkereket, azokat amit filebeolvasahoz hasznaltunk
         } else {
             valueIsValid = true;
         }
