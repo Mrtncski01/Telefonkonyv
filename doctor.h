f54e1802f340ca1ff0643d47bcf0bc5f51763358
Mrtncski01 <150520755+Mrtncski01@users.noreply.github.com>
2025-05-17 11:35:31 +0200

    Update doctor.h

--- doctor.h
+++ doctor.h
@@ -2,21 +2,22 @@
 #define PHONEBOOK_DOCTOR_H
 #include "contact.h"
 
+//doctor osztaly contactbol szarmazva
 class Doctor : public Contact {
 private:
-    std::string specialization;
-    std::string workplace;
+    std::string specialization; //specializacio
+    std::string workplace; //munkahely
 public:
     Doctor(const Name& newName, const std::string& newPrivatePhone,
            const std::string& newWorkPhone, const std::string& newAddress,
            const std::string& newWorkplace, const std::string& newSpecialization) : Contact(newName, newPrivatePhone, newWorkPhone, newAddress),
-                                                                                    specialization(newSpecialization), workplace(newWorkplace) {}
-    std::string getSpecialization() const;
-    std::string getWorkplace() const;
-    void setSpecialization(const std::string newSpecialization);
-    void setWorkplace(const std::string newWorkplace);
-    void print(std::ostream& os) const;
-    std::string serialize() const;
+                                                                                    specialization(newSpecialization), workplace(newWorkplace) {} //konstruktor
+    std::string getSpecialization() const; //visszaadja a specializaciot
+    std::string getWorkplace() const; //visszaadja a munkahelyet
+    void setSpecialization(const std::string newSpecialization); //beallitas, modositashoz
+    void setWorkplace(const std::string newWorkplace); //ugyanaz
+    void print(std::ostream& os) const; //kiirja a doctor adatait
+    std::string serialize() const; //filebamentes
     //~Doctor() {}
 };
 std::ostream& operator<<(std::ostream& os, const Doctor& d);
