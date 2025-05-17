e68e7e7b0963021bda045f9cdb85fafe2a7e8183
Mrtncski01 <150520755+Mrtncski01@users.noreply.github.com>
2025-05-17 11:34:24 +0200

    Update contact.h

--- contact.h
+++ contact.h
@@ -3,29 +3,30 @@
 #include "name.h"
 #include <iostream>
 
+//contact osztaly, ebbol szarmazik le a doctor es a patient, alapadatokat tartalmaz, absztrakt osztaly
 class Contact {
 protected:
-    Name name;
-    std::string privatePhone;
-    std::string workPhone;
-    std::string address;
+    Name name; //nev
+    std::string privatePhone; //privat szam
+    std::string workPhone; //munkaelyi szam
+    std::string address; //cim
 public:
     Contact(const Name& newName,
             const std::string& newPrivatePhone,
             const std::string& newWorkPhone,
-            const std::string& newAddress) : name(newName), privatePhone(newPrivatePhone), workPhone(newWorkPhone), address(newAddress) {}
-    Name& getName() ;
-    std::string getAddress() const;
-    std::string getWorkPhone() const;
-    std::string getPrivatePhone() const;
-    void setAddress(std::string newAddress);
-    void setWorkPhone(std::string newWorkPhone);
-    void setPrivatePhone(std::string newPrivatePhone);
-    bool operator==(const std::string& s) const;
-    virtual void print(std::ostream& os) const = 0;
-    virtual std::string serialize() const = 0;
-    static Contact* deserialize(const std::string& line);
-    virtual ~Contact() {}
+            const std::string& newAddress) : name(newName), privatePhone(newPrivatePhone), workPhone(newWorkPhone), address(newAddress) {} //konstruktor
+    Name& getName() ; //visszaadja a nevet
+    std::string getAddress() const; //visszaadja cimet
+    std::string getWorkPhone() const; //visszaadja a munkahelyi szamot
+    std::string getPrivatePhone() const; //visszaadja a privat szamot
+    void setAddress(std::string newAddress); //modositashoz kell beallitja a cimet
+    void setWorkPhone(std::string newWorkPhone); //ugyanez
+    void setPrivatePhone(std::string newPrivatePhone); //ugyanez
+    bool contactMatches(const std::string& s, int userChoice) const; //kereseshez, kell, visszaadja hogy megegyezik-e a string, a contact valamelyik attributumaval a valasztott opcionak megfeleloen
+    virtual void print(std::ostream& os) const = 0; //contact adatai kiiratasa, virtualis, a patient meg a doctor felulirja
+    virtual std::string serialize() const = 0; // filebamentes, ezt is felulirjak a leszarmazottak
+    static Contact* deserialize(const std::string& line); //fuggvenybol kiolvasas sorszamnak megfeleloen, contact-0 doctor-1
+    virtual ~Contact() {} //destruktor
 };
 
 
